31ac2a851b47808bd5d6c4f7989395a35bbe105a
franko05-min <franko05@libero.it>
2025-07-27 22:01:40 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class TradingApp(App):
-    def build(self):
-        return Label(text='Hello Trading World!')
-
-if __name__ == '__main__':
-    TradingApp().run()
